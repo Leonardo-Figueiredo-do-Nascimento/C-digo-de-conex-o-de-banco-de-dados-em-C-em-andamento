e4a074c5a21a7fe776cd4e74e3cafc355bd09128
Leonardo-Figueiredo-do-Nascimento <kriptonascimento@hotmail.com>
2023-03-01 15:00:34 -0300

    Codigo de CRUD em C# em andamento

--- Teste crud/Conexão/DAOUsuario.cs
+++ Teste crud/Conexão/DAOUsuario.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Teste_crud.Modelos;
+using System.Collections.Generic;
 
 namespace Teste_crud.Conexão
 {
@@ -36,5 +37,45 @@ namespace Teste_crud.Conexão
             Console.WriteLine("Usuario deletado com sucesso!");
             conn.Close();
         }
+
+        public void alterarNome(Usuario usuario, string nome)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-BSA3PC1\\SQLEXPRESS; Initial Catalog=Teste_crud; Integrated Security=true;");
+            conn.Open();
+
+            string update = "UPDATE tb_usuario SET nome ="+nome.ToString()+" WHERE id = '" + usuario.Id.ToString() + "'";
+
+            SqlCommand cmd = new SqlCommand(update, conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Nome alterado com sucesso!");
+            conn.Close();
+        }
+
+        public void listar(Usuario usuario)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-BSA3PC1\\SQLEXPRESS; Initial Catalog=Teste_crud; Integrated Security=true;");
+            conn.Open();
+
+            string listar = "SELECT * FROM tb_usuario'" + usuario.Nome.ToString() + "'";
+
+            SqlCommand cmd = new SqlCommand(listar, conn);
+            cmd.ExecuteNonQuery();
+
+            List<Usuario> ls = new List<Usuario>();
+
+
+            foreach(Usuario u in ls)
+            {
+                Console.WriteLine("id: "+ u.Id);
+                Console.WriteLine("nome: "+u.Nome);
+                Console.WriteLine("idade: "+u.Idade);
+                Console.WriteLine("endereço: "+u.Endereço);
+            }
+
+            
+            conn.Close();
+            Console.ReadLine();
+        }
+
     }
 }
